83fb8bdf81ef12a1e8551bcecacbfc1da2330afa
Muhammad Syafiq Bin Zainuddin <tmukmkd@gmail.com>
2023-08-17 08:55:49 +0800

    prep generator.

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,21 @@
+
+<li class="nav-item">
+    <a href="{{ route('zz/Categories.index') }}" class="nav-link {{ Request::is('zz/Categories*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Zz/Categories</p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="{{ route('zzCategories.index') }}" class="nav-link {{ Request::is('zzCategories*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Zz Categories</p>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="{{ route('zz/Categories.index') }}" class="nav-link {{ Request::is('zz/Categories*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Zz/ Categories</p>
+    </a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('zz/categories', App\Http\Controllers\Zz\CategoryController::class);
+
+Route::resource('zz-categories', App\Http\Controllers\ZzCategoryController::class);
+Route::resource('zz/-categories', App\Http\Controllers\Zz/CategoryController::class);
